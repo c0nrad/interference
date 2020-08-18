92ace35d7eee6288cbb25ce3302cc74367e839ff
Stuart Larsen <c0nrad@c0nrad.io>
2020-08-18 18:39:36 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,140 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Decimal } from 'decimal.js'
+import { BaseChartDirective, Label, Color } from 'ng2-charts';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { UtilService } from './util.service'
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+    <div class="row">
+      <div class="col-8">
+        <div style="display: block;">
+        <canvas baseChart [datasets]="lineChartData"
+                    [labels]="lineChartLabels"
+                    [colors]="lineChartColors"
+                    [options]="lineChartOptions"
+                    chartType="line"></canvas>
+                    </div>
+
+      </div>
+      <div class="col-4">
+        <div class="form-group">
+          <label>Wavelength [nm]: {{wavelength_nm}}</label>
+          <input type="range" min=380 max=700 (change)="render()" [(ngModel)]="wavelength_nm" class="input-control">
+        </div>
+
+        <div class="form-group">
+          <label>Slit Width [nm]: {{slitWidth_nm}}</label>
+          <input type="range" min=100000 max=1000000 (change)="render()" [(ngModel)]="slitWidth_nm" class="input-control">
+        </div>
+
+        <div class="form-group">
+          <label>Wall Distance [m]: {{distance_m}}</label>
+          <input type="range" min=1 max=10 (change)="render()" [(ngModel)]="distance_m" class="input-control">
+        </div>
+      </div>
     </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
   `,
   styles: []
 })
 export class AppComponent {
   title = 'interference';
+  wavelength_nm = 650;
+  slitWidth_nm = 250000;
+  distance_m = 4;
+
+  public lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+  ];
+  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions: (ChartOptions) = {
+    responsive: true,
+    scales: {
+      // We use this empty structure as a placeholder for dynamic theming.
+      xAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: "Interference Pattern [mm]"
+        }
+      }],
+      yAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: "Intensity"
+        }
+      }],
+    },
+    legend: { display: false }
+  }
+  public lineChartColors: Color[] = [
+    {
+      backgroundColor: 'rgba(148,159,177,0.2)',
+    }
+  ]
+  @ViewChild(BaseChartDirective, { static: true }) chart?: BaseChartDirective;
+
+  constructor(private utilService: UtilService) {
+    this.render()
+  }
+
+  render() {
+    let data = []
+    let labels = []
+    for (let h = -25000000; h < 25000000; h += 100000) {
+      let offset = this.phaseOffset1(h).sub(this.phaseOffset2(h)).mod(2 * Math.PI)
+      let r = (this.l2(h).add(this.l2(h)).div(2)).div(1000000000)
+
+      let intensity = Decimal.cos(offset).pow(2)
+      // let intensity = Decimal.sin(offset).div(offset).pow(2)
+      data.push(intensity.abs().toNumber())
+      labels.push((h / 1000000).toString())
+    }
+    this.lineChartData[0].data = data
+    this.lineChartLabels = labels
+    this.lineChartColors[0].backgroundColor = this.utilService.wavelengthToColor(this.wavelength_nm)[0] as string
+    this.chart?.update()
+  }
+
+  theta(n: number): number {
+    return Math.asin(n * this.wavelength_nm / this.distance_m)
+  }
+
+  l1(h_nm: number): Decimal {
+    let d = new Decimal(this.distance_m).mul(1000000000)
+    let h = new Decimal(h_nm - this.slitWidth_nm / 2)
+    return Decimal.sqrt(d.pow(2).add(h.pow(2)))
+  }
+
+  l2(h_nm: number) {
+    let d = new Decimal(this.distance_m).mul(1000000000)
+    let h = new Decimal(h_nm + this.slitWidth_nm / 2)
+    return Decimal.sqrt(d.pow(2).add(h.pow(2)))
+  }
+
+  phaseOffset1(h_nm: number) {
+    return this.l1(h_nm).mod(this.wavelength_nm).div(this.wavelength_nm).mul(2 * Math.PI)
+  }
+
+  phaseOffset2(h_nm: number) {
+    return this.l2(h_nm).mod(this.wavelength_nm).div(this.wavelength_nm).mul(2 * Math.PI)
+  }
+
+  //   <table class="table">
+  //   <tr>
+  //     <th>h [nm]</th>
+  //     <th>l1 [nm]</th>
+  //     <th>l2 [nm]</th>
+  //     <th>phase1</th>
+  //     <th>phase2</th>
+  //   </tr>
+
+  //   <tr *ngFor="let h_nm of [100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000]">
+  //     <td>{{h_nm}}</td>
+  //     <td>{{l1(h_nm)}}</td>
+  //     <td>{{l2(h_nm)}}</td>
+  //     <td>{{phaseOffset1(h_nm)}}</td>          
+  //     <td>{{phaseOffset2(h_nm)}}</td>  
+  //   </tr>
+  // </table>
 }
---
+++ src/app/util.service.ts
@@ -0,0 +1,70 @@
+// @ts-nocheck
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilService {
+
+  constructor() { }
+
+  wavelengthToColor(wavelength: number) {
+    var r,
+      g,
+      b,
+      alpha,
+      colorSpace,
+      wl = wavelength,
+      gamma = 1, R, G, B;
+
+    if (wl >= 380 && wl < 440) {
+      R = -1 * (wl - 440) / (440 - 380);
+      G = 0;
+      B = 1;
+    } else if (wl >= 440 && wl < 490) {
+      R = 0;
+      G = (wl - 440) / (490 - 440);
+      B = 1;
+    } else if (wl >= 490 && wl < 510) {
+      R = 0;
+      G = 1;
+      B = -1 * (wl - 510) / (510 - 490);
+    } else if (wl >= 510 && wl < 580) {
+      R = (wl - 510) / (580 - 510);
+      G = 1;
+      B = 0;
+    } else if (wl >= 580 && wl < 645) {
+      R = 1;
+      G = -1 * (wl - 645) / (645 - 580);
+      B = 0.0;
+    } else if (wl >= 645 && wl <= 780) {
+      R = 1;
+      G = 0;
+      B = 0;
+    } else {
+      R = 0;
+      G = 0;
+      B = 0;
+    }
+
+    // intensty is lower at the edges of the visible spectrum.
+    if (wl > 780 || wl < 380) {
+      alpha = 0;
+    } else if (wl > 700) {
+      alpha = (780 - wl) / (780 - 700);
+    } else if (wl < 420) {
+      alpha = (wl - 380) / (420 - 380);
+    } else {
+      alpha = 1;
+    }
+
+    colorSpace = ["rgba(" + (R * 100) + "%," + (G * 100) + "%," + (B * 100) + "%, " + alpha + ")", R, G, B, alpha]
+
+    // colorSpace is an array with 5 elements.
+    // The first element is the complete code as a string.  
+    // Use colorSpace[0] as is to display the desired color.  
+    // use the last four elements alone or together to access each of the individual r, g, b and a channels.  
+
+    return colorSpace;
+  }
+}
